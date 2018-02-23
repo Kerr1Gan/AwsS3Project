03c57259d6cd8aa434313ffb4f95df8dd659da32
Kerr1Gan <674712964@qq.com>
2018-02-23 23:33:18 +0800

    dev

--- selenium/src/main/java/org/ecjtu/selenium/ofo91/CrawlOfO91.java
+++ selenium/src/main/java/org/ecjtu/selenium/ofo91/CrawlOfO91.java
@@ -2,6 +2,7 @@ package org.ecjtu.selenium.ofo91;
 
 import org.ecjtu.selenium.SeleniumEngine;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchWindowException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -19,21 +20,50 @@ public class CrawlOfO91 {
         ChromeDriver chromeDriver = SeleniumEngine.getInstance().newDesktopChromeDriver();
         Map<String, List<OfO91Model>> map = new LinkedHashMap<>();
         try {
-            chromeDriver.get("http://www.ofo91.com/");
-            WebElement menu = chromeDriver.findElement(By.className("main_nav"));
+            WebElement menu = null;
+            while (true) {
+                try {
+                    chromeDriver.get("http://www.ofo91.com/");
+                    Thread.sleep(500);
+                    menu = chromeDriver.findElement(By.className("main_nav"));
+                } catch (NoSuchWindowException e) {
+                    break;
+                } catch (Exception e) {
+                    chromeDriver.quit();
+                    chromeDriver = SeleniumEngine.getInstance().newDesktopChromeDriver();
+                    continue;
+                }
+                break;
+            }
             List<WebElement> webElements = menu.findElements(By.tagName("a"));
             List<String> menuUrls = new ArrayList<>();
             List<String> titleList = new ArrayList<>();
             for (WebElement element : webElements) {
-                menuUrls.add("http://www.ofo91.com/" + element.getAttribute("href"));
+                menuUrls.add(element.getAttribute("href"));
                 titleList.add(element.getText());
             }
             int index = 0;
             for (String title : titleList) {
                 List<OfO91Model> list = new ArrayList<>();
-                chromeDriver.get(menuUrls.get(index++));
+                List<WebElement> pages = null;
+                int flagIndex = 0;
+                while (true) {
+                    if (flagIndex++ > 20) {
+                        break;
+                    }
+                    try {
+                        chromeDriver.get(menuUrls.get(index));
+                        pages = chromeDriver.findElement(By.className("pagination")).findElements(By.tagName("li"));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        chromeDriver.quit();
+                        chromeDriver = SeleniumEngine.getInstance().newDesktopChromeDriver();
+                        continue;
+                    }
+                    break;
+                }
+                index++;
                 List<String> pageList = new ArrayList<>();
-                List<WebElement> pages = chromeDriver.findElement(By.className("pagination")).findElements(By.tagName("li"));
                 pages.remove(0);
                 pages.remove(pages.size() - 1);
                 String baseUrl = pages.get(pages.size() - 1).findElement(By.tagName("a")).getAttribute("href");
@@ -43,13 +73,29 @@ public class CrawlOfO91 {
                     pageList.add(url);
                 }
                 for (String page : pageList) {
-                    chromeDriver.get(page);
-                    WebElement element = chromeDriver.findElement(By.className("red_video_sec"));
+                    WebElement element = null;
+                    while (true) {
+                        try {
+                            chromeDriver.get(page);
+                            element = chromeDriver.findElement(By.className("red_video_sec"));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            chromeDriver.quit();
+                            chromeDriver = SeleniumEngine.getInstance().newDesktopChromeDriver();
+                            continue;
+                        }
+                        break;
+                    }
                     List<WebElement> elements = element.findElements(By.tagName("a"));
                     for (int i = 0; i < elements.size(); i++) {
                         String videoTitle = elements.get(i).getText();
                         String videoUrl = elements.get(i).getAttribute("href");
-                        WebElement bgElement = elements.get(i).findElement(By.className("red_video_card"));
+                        WebElement bgElement = null;
+                        try {
+                            bgElement = elements.get(i).findElement(By.className("red_video_card"));
+                        } catch (Exception e) {
+                        }
+                        if (bgElement == null) continue;
                         String style = bgElement.getAttribute("style");
                         StringBuilder image = new StringBuilder();
                         boolean flag = false;
--- selenium/src/main/java/org/ecjtu/selenium/ofo91/CrawlOfO91Videos.java
+++ selenium/src/main/java/org/ecjtu/selenium/ofo91/CrawlOfO91Videos.java
@@ -1,6 +1,8 @@
 package org.ecjtu.selenium.ofo91;
 
 import org.ecjtu.selenium.SeleniumEngine;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchWindowException;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.FileInputStream;
@@ -23,19 +25,28 @@ public class CrawlOfO91Videos {
         ChromeDriver driver = null;
         if (map != null) {
             Set<Map.Entry<String, List<OfO91Model>>> entrySet = map.entrySet();
+            SeleniumEngine.initEngine(SeleniumEngine.DRIVE_PATH);
+            driver = SeleniumEngine.getInstance().newDesktopChromeDriver();
+            loop:
             for (Map.Entry<String, List<OfO91Model>> entry : entrySet) {
                 List<OfO91Model> ofoList = entry.getValue();
                 for (OfO91Model model : ofoList) {
                     if (model.getInnerVideoUrl() == null || model.getInnerVideoUrl().equals("")) {
-                        SeleniumEngine.initEngine(SeleniumEngine.DRIVE_PATH);
-                        driver = SeleniumEngine.getInstance().newDesktopChromeDriver();
-                        try {
-                            driver.get(model.getVideoUrl());
-                            // get the inner video url
-                            String innerVideoUrl = "";
-                            model.setInnerVideoUrl(innerVideoUrl);
-                        } catch (Exception e) {
-                            e.printStackTrace();
+                        while (true) {
+                            try {
+                                driver.get(model.getVideoUrl());
+                                // get the inner video url
+                                String innerVideoUrl = driver.findElement(By.id("playerv")).getAttribute("src");
+                                model.setInnerVideoUrl(innerVideoUrl);
+                            } catch (NoSuchWindowException e) {
+                                break loop;
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                driver.quit();
+                                driver = SeleniumEngine.getInstance().newDesktopChromeDriver();
+                                continue;
+                            }
+                            break;
                         }
                     }
                 }
