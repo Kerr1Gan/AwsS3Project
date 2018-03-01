a182fd0d11451d5fce27ae22f7aad4de7ccc9bf8
Kerr1Gan <674712964@qq.com>
2018-03-01 09:23:40 +0800

    dev

--- selenium/src/main/java/org/ecjtu/selenium/ofo91/CrawlOfO91Videos.java
+++ selenium/src/main/java/org/ecjtu/selenium/ofo91/CrawlOfO91Videos.java
@@ -1,14 +1,15 @@
 package org.ecjtu.selenium.ofo91;
 
 import org.ecjtu.selenium.SeleniumEngine;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchWindowException;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -22,6 +23,44 @@ public class CrawlOfO91Videos {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        String jsonString = null;
+        try (FileInputStream is = new FileInputStream("res\\ofo91.json")) {
+            ByteArrayOutputStream buf = new ByteArrayOutputStream();
+            byte[] temp = new byte[1024 * 10];
+            int len = 0;
+            while ((len = is.read(temp)) > 0) {
+                buf.write(temp, 0, len);
+            }
+            jsonString = buf.toString("utf-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        List<String[]> cache = new ArrayList<>();
+        if (jsonString != null) {
+            try {
+                JSONArray oldArray = new JSONArray(jsonString);
+                for (int i = 0; i < oldArray.length(); i++) {
+                    JSONObject oldObj = (JSONObject) oldArray.get(i);
+                    JSONArray oldArr = (JSONArray) oldObj.get("array");
+                    for (int j = 0; j < oldArr.length(); j++) {
+                        JSONObject obj = oldArr.getJSONObject(j);
+                        String videoUrl = obj.optString("videoUrl");
+                        String innerVideoUrl = obj.optString("innerVideoUrl");
+                        if (videoUrl != null && innerVideoUrl != null && !videoUrl.equals("") && !innerVideoUrl.equals("")) {
+                            String[] cacheItem = new String[2];
+                            cacheItem[0] = videoUrl;
+                            cacheItem[1] = innerVideoUrl;
+                            cache.add(cacheItem);
+                        }
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
         ChromeDriver driver = null;
         if (map != null) {
             Set<Map.Entry<String, List<OfO91Model>>> entrySet = map.entrySet();
@@ -32,10 +71,20 @@ public class CrawlOfO91Videos {
                 List<OfO91Model> ofoList = entry.getValue();
                 for (OfO91Model model : ofoList) {
                     if (model.getInnerVideoUrl() == null || model.getInnerVideoUrl().equals("")) {
+                        inner:
                         while (true) {
                             try {
-                                driver.get(model.getVideoUrl());
+                                //load from cache
+                                for (String[] item : cache) {
+                                    if (item[0].equals(model.getVideoUrl())) {
+                                        model.setInnerVideoUrl(item[1]);
+                                        break inner;
+                                    }
+                                }
+
+
                                 // get the inner video url
+                                driver.get(model.getVideoUrl());
                                 String innerVideoUrl = driver.findElement(By.id("playerv")).getAttribute("src");
                                 model.setInnerVideoUrl(innerVideoUrl);
                             } catch (NoSuchWindowException e) {
