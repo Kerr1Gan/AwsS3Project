45b8a108774f89f2a8559d7faea22c7807bbe6d1
Kerr1Gan <674712964@qq.com>
2018-03-03 09:56:13 +0800

    dev

--- src/main/java/org/ecjtu/s3/MakeImageByFFmpeg.java
+++ src/main/java/org/ecjtu/s3/MakeImageByFFmpeg.java
@@ -10,6 +10,7 @@ import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.Date;
 
 public class MakeImageByFFmpeg {
@@ -22,9 +23,11 @@ public class MakeImageByFFmpeg {
         ObjectListing objectListing = s3.listObjects(new ListObjectsRequest()
                 .withBucketName("firststorage0001"));
         Runtime runtime = Runtime.getRuntime();
+        Calendar endDate = Calendar.getInstance();
+        endDate.add(Calendar.HOUR, 1);
         for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
             System.out.print(" - " + objectSummary.getKey());
-            String url = s3.generatePresignedUrl("firststorage0001", objectSummary.getKey(), new Date()).toString();
+            String url = s3.generatePresignedUrl("firststorage0001", objectSummary.getKey(), endDate.getTime()).toString();
             String key = "image_" + objectSummary.getKey();
             try {
                 runtime.exec(String.format("ffmpeg -y -ss 300 -t 5 -i %s -vf \"select=eq(pict_type\\\\,I)\" -vframes 1 -f image2 -s 250x140 %s", url, key));
