5d528a4539b073fe374508ed2217ae33fe895374
Kerr1Gan <674712964@qq.com>
2019-07-04 00:37:58 +0800

    dev

--- selenium/src/main/java/org/ecjtu/selenium/gpub/Main.java
+++ selenium/src/main/java/org/ecjtu/selenium/gpub/Main.java
@@ -5,11 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.IOException;
-import java.util.function.Function;
 
 public class Main {
 
@@ -26,6 +23,7 @@ public class Main {
                     if (webElement != null) {
                         int x = 0;
                         x++;
+                        fillStoreListing(driver);
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -39,12 +37,70 @@ public class Main {
 
     public static void uploadFile(String path) {
         try {
-            Process process = Runtime.getRuntime().exec("F:\\Projects\\AwsS3Project\\selenium\\src\\libs\\upload.exe "+path);
+            Process process = Runtime.getRuntime().exec("F:\\Projects\\AwsS3Project\\selenium\\src\\libs\\upload.exe " + path);
             process.waitFor();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void fillStoreListing(WebDriver driver) {
+        // short description
+        driver.findElements(By.tagName("textarea")).get(0).sendKeys("short description");
+
+        // full description
+        driver.findElements(By.tagName("textarea")).get(1).sendKeys("full description");
+
+        driver.findElements(By.className("gwt-TextBox")).get(2).sendKeys("");
+
+        // choose applicaion app
+        driver.findElements(By.className("A1JAINC-Q-a")).get(0).click();
+        driver.findElements(By.className("A1JAINC-P-k")).get(0).click();
+
+        // choose finance app
+        driver.findElements(By.className("A1JAINC-Q-a")).get(3).click();
+        driver.findElements(By.className("A1JAINC-P-k")).get(0).click();
+
+        // enter mail
+        driver.findElements(By.className("gwt-TextBox")).get(4).sendKeys("ethanxiang95@gmail.com");
+
+        // enter privacy url
+        driver.findElements(By.className("gwt-TextBox")).get(6).sendKeys("https://www.baidu.com");
+
+        // upload 512 logo
+        driver.findElements(By.tagName("input")).get(4).sendKeys("C:\\Users\\xiang\\Desktop\\SB\\512.png");
+
+        // upload screen capture index 7 - 14
+        driver.findElements(By.tagName("input")).get(7).sendKeys("C:\\Users\\xiang\\Desktop\\SB\\512.png");
+
+        // upload 1024 * 500 7+5
+        driver.findElements(By.tagName("input")).get(13).sendKeys("C:\\Users\\xiang\\Desktop\\SB\\512.png");
+
+        try {
+            Thread.sleep(10 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            // save store detail
+            driver.findElement(By.id("gwt-uid-642")).click();
+        }
+    }
+
+    public static void fillApkInfo(WebDriver driver) {
+        // upload apk
+        driver.findElements(By.tagName("input")).get(17).sendKeys("C:\\Users\\xiang\\Desktop\\SB\\SB.zip");
+
+        // set publish message
+        driver.findElements(By.tagName("textarea")).get(1).clear();
+        driver.findElements(By.tagName("textarea")).get(1).sendKeys("<en-US>\n请在此处输入或粘贴en-US版的版本说明\n</en-US>");
+
+        // save app release
+        driver.findElements(By.tagName("button")).get(78).click();
+    }
+
+    public static void fillAppContent(WebDriver driver) {
+
     }
 }
